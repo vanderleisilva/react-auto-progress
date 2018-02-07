04c96f041f254191926c834922633b125d2397de
Vanderlei Alves da Silva <vanderlei.alves.da.silva@gmail.com>
2018-02-07 15:33:46 -0200

    add component and demo page

--- demo/src/index.js
+++ demo/src/index.js
@@ -1,15 +1,34 @@
 import React, {Component} from 'react'
 import {render} from 'react-dom'
+import axios from 'axios';
 
-import Example from '../../src'
+import AutoLoader from '../../src'
 
 class Demo extends Component {
-  render() {
-    return <div>
-      <h1>react-auto-progress Demo</h1>
-      <Example/>
-    </div>
-  }
+
+	constructor(props) {
+		super(props);
+		this.get = this.get.bind(this);
+	}
+
+	state = { data : ''}
+
+	get(){
+		const apiUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+		const handler = data => this.setState({ data: JSON.stringify(data) });
+
+		axios.get(apiUrl).then(handler).catch(handler);
+	}
+
+	render() {
+	return <div>
+			<AutoLoader/>
+			<h1>react-auto-progress Demo</h1>
+			<p>Just add the component and forget about all the rest</p>
+			<button onClick={this.get}>Make my ajax requisition</button>
+			<p>{this.state.data}</p>
+		</div>
+	}
 }
 
 render(<Demo/>, document.querySelector('#demo'))
--- src/index.js
+++ src/index.js
@@ -1,9 +1,36 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+import 'nprogress/nprogress.css';
+import NProgress from 'nprogress';
 
 export default class extends Component {
-  render() {
-    return <div>
-      <h2>Welcome to React components</h2>
-    </div>
-  }
+
+	componentDidMount() {
+		var listener = { 
+			tempOpen: XMLHttpRequest.prototype.open,
+			tempSend: XMLHttpRequest.prototype.send,
+			callback(){ }
+		};
+
+		XMLHttpRequest.prototype.open = function(a='',b='') {
+			NProgress.start();
+			listener.tempOpen.apply(this, arguments);
+			listener.method = a;  
+			listener.url = b;
+			if (a.toLowerCase() == 'get') {
+				listener.data = b.split('?');
+				listener.data = listener.data[1];
+			}
+		}
+
+		XMLHttpRequest.prototype.send = function(a='',b='') {
+			setTimeout(() => { NProgress.done() }, 500);
+			listener.tempSend.apply(this, arguments);
+			if(listener.method.toLowerCase() == 'post') { listener.data = a };
+			listener.callback();
+		}
+	}
+
+	render() {
+		return false;
+	}
 }
